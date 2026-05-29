99cd5ac18e893e57959f383da02389ea8064a523
rohrrafael1 <rohr.rafa@icloud.com>
2026-05-29 13:07:10 -0300

    feat: dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-white text-zinc-900 selection:bg-zinc-200 dark:bg-zinc-900 dark:text-zinc-50 dark:selection:bg-zinc-800">
+      <body className="min-h-full flex flex-col bg-white text-zinc-900 selection:bg-zinc-200 dark:bg-black dark:text-zinc-50 dark:selection:bg-zinc-800">
         <Providers>
           <Navbar />
           <main className="flex-1">{children}</main>
